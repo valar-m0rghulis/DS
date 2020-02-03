6ab0594c8b91e7a2ef991f83eaa0f955d52cf7e4
deepak <t3zcatlipoca@protonmail.com>
2020-02-03 17:10:26 +0530

    commit

---
+++ List/single.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+
+using namespace std;
+
+class node{
+    private:
+    int data;
+    node* next;
+
+    public:
+
+    void insert_at_begining(){
+        
+    }
+
+};
+
+int main(){
+
+    return 0;
+}
